4546dbc579e24cb74231c442f3f351bacb5596f2
Lutik <share.lithium@gmail.com>
2010-02-07 18:08:54 +0300

    Fixed bug with "undying" players.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1320,6 +1320,10 @@ void Player::Update( uint32 p_time )
         if (!m_regenTimer)
             RegenerateAll();
     }
+    else if(!HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_GHOST))
+    {
+        SetHealth(0);
+    }
 
     if (m_deathState == JUST_DIED)
     {
